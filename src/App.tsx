9cdd4c142d63e70850f3d4bf9e7d1f348f7fae67
MineChannel5816 <danielefinocchiaro@keix.com>
2021-07-14 10:29:20 +0200

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,311 +1,242 @@
-import React, { useState, useCallback } from 'react';
-import _ from 'lodash/fp';
+import React, { useState, useEffect } from 'react';
+
+import { GraphQLClient, gql } from 'graphql-request';
+
+import __ from 'lodash/fp';
+import _ from 'lodash';
+/*;
+import { v4 } from 'uuid';
+
+import * as Blueprint from '@blueprintjs/core';
+import * as Icons from 'react-icons/all'; 
+
+import 'normalize.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+*/
 import classNames from 'classnames';
+import { DateTime } from 'luxon';
 
-interface Event {
-  status:
-    | 'LAMPADINA_SPENTA'
-    | 'LAMPADINA_ACCESA'
-    | 'CONTATORE_SPENTO'
-    | 'CONTATORE_ACCESO';
-  timestamp: Date;
+interface Users {
+  items: User[];
 }
 
-interface Command {
-  command:
-    | 'SPEGNI_LAMPADINA'
-    | 'ACCENDI_LAMPADINA'
-    | 'SPEGNI_GENERATORE'
-    | 'ACCENDI_GENERATORE';
-  timestamp: Date;
+interface User {
+  userId: string;
 }
 
-interface Button {
-  onClick: () => void;
+interface UserTransactions {
+  items: UserTransaction[];
 }
 
-interface Counter {
-  seconds: number;
+interface UserTransaction {
+  id: string;
+  amount: number;
+  userId: string;
+  time: string;
+  creditDate: string;
+  delayed: boolean;
 }
 
-/* function Counter(p: Counter) {
-  return <div>Seconds in the last minute: {p.seconds}</div>;
-}
- */
-function BtnOff(p: Button) {
-  const style = classNames(
-    'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-red-500',
-  );
-
-  return (
-    <button
-      className={style}
-      //disabled={state.status === 'stopped' ? true : false}
-      onClick={() => p.onClick()}
-    >
-      Spento
-    </button>
-  );
+async function getQuery(str: string) {
+  const endpoint = 'https://dev.graphql-v2.keix.com/graphql';
+  const query = gql`
+    ${str}
+  `;
+  // ... or create a GraphQL client instance to send requests
+  const client = new GraphQLClient(endpoint, { headers: {} });
+  const graph = client.request(query).then((data) => {
+    return data;
+  });
+  return await graph;
 }
 
-function BtnOn(p: Button) {
-  const style = classNames(
-    'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-green-500',
-  );
-
-  return (
-    <button
-      className={style}
-      //disabled={state.status === 'started' ? true : false}
-      onClick={() => p.onClick()}
-    >
-      Acceso
-    </button>
-  );
-}
-
-function BtnGeneratorOff(p: Button) {
-  const style = classNames(
-    'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-blue-500',
-  );
-
-  return (
-    <button
-      className={style}
-      //disabled={state.status === 'started' ? true : false}
-      onClick={() => p.onClick()}
-    >
-      Contatore OFF
-    </button>
-  );
-}
-
-function BtnGeneratorOn(p: Button) {
-  const style = classNames(
-    'px-3 py-2 mr-2 rounded disabled:opacity-50 disabled:cursor-not-allowed bg-purple-500',
-  );
-
-  return (
-    <button
-      className={style}
-      //disabled={state.status === 'started' ? true : false}
-      onClick={() => p.onClick()}
-    >
-      Contatore ON
-    </button>
-  );
-}
-
-function Lampada(p: Event[]) {
-  if (Object.keys(p).length > 0) {
-    let actualStatus = p[Object.keys(p).length - 1];
-    const style = classNames(
-      'flex flex-grow justify-center h-20',
-      actualStatus.status === 'LAMPADINA_ACCESA'
-        ? 'bg-yellow-500'
-        : 'bg-gray-500',
-    );
-    return <div className={style}>{actualStatus.status}</div>;
-  } else {
-    const style = classNames('flex flex-grow justify-center h-20 bg-gray-500');
-    return <div className={style}></div>;
-  }
-}
-
-function isLampadinaEvent(event: Event) {
-  return (
-    event.status === 'LAMPADINA_SPENTA' || event.status === 'LAMPADINA_ACCESA'
-  );
-}
-
-function isContatoreEvent(event: Event) {
-  return (
-    event.status === 'CONTATORE_SPENTO' || event.status === 'CONTATORE_ACCESO'
-  );
+async function doMutation(str: string) {
+  const endpoint = 'https://dev.graphql-v2.keix.com/graphql';
+  // ... or create a GraphQL client instance to send requests
+  const client = new GraphQLClient(endpoint, { headers: {} });
+  const mutation = gql`
+    mutation AddMovie($title: String!, $releaseDate: Int!) {
+      insert_movies_one(object: { title: $title, releaseDate: $releaseDate }) {
+        title
+        releaseDate
+      }
+    }
+  `;
+  const graph = client.request(mutation).then((data) => {
+    return data;
+  });
+  return await graph;
 }
 
 function App() {
-  const [events, setEvents] = useState<Event[]>([
-    { status: 'CONTATORE_ACCESO', timestamp: new Date() },
-  ]);
-
-  const [commands, setCommand] = useState<Command[]>([]);
-
-  const setLightOn = useCallback(() => {
-    let LampadinaEvent = events.filter(isLampadinaEvent);
-    let ContatoreEvent = events.filter(isContatoreEvent);
-
-    let actualStatusCont = ContatoreEvent[ContatoreEvent.length - 1];
-    if (LampadinaEvent.length > 0) {
-      let actualStatusLamp = LampadinaEvent[LampadinaEvent.length - 1];
-      if (
-        actualStatusLamp.status === 'LAMPADINA_SPENTA' &&
-        actualStatusCont.status === 'CONTATORE_ACCESO'
-      ) {
-        setEvents([
-          ...events,
-          { status: 'LAMPADINA_ACCESA', timestamp: new Date() },
-        ]);
+  const [dataTransaction, setDataTransaction] = useState<UserTransactions>({
+    items: [],
+  });
+  const [dataUser, setDataUser] = useState<Users>({ items: [] });
+  const [userBalance, setUserBalance] = useState(0);
+  const [userSelected, setUserSelected] = useState('all');
+  useEffect(async () => {
+    if (userSelected === 'all') {
+      const result1 = await getQuery(`
+      query {
+        userTransactions(limit: 100) {
+          items {
+            id
+            amount
+            userId
+            time
+            creditDate
+            delayed
+          }
+        }
       }
+    `);
+      const result2 = await getQuery(`query{userTransactions{items{userId}}}`);
+      setDataTransaction(result1.userTransactions);
+      setDataUser(result2.userTransactions);
     } else {
-      if (actualStatusCont.status === 'CONTATORE_ACCESO') {
-        setEvents([
-          ...events,
-          { status: 'LAMPADINA_ACCESA', timestamp: new Date() },
-        ]);
-      }
+      const result1 = await getQuery(`query{
+        userTransactions(filters:{filters:[{attributeName:"userId",filters:[{op:"=",value:"${userSelected}"}]}]}) {
+          items{
+            id
+            amount
+            userId
+            time
+            creditDate
+            delayed
+          }
+        }
+      }`);
+      const result2 = await getQuery(`query{userTransactions{items{userId}}}`);
+      const result3 = await getQuery(
+        `query{getUserBalance(id:"${userSelected}")}`,
+      );
+      setUserBalance(result3.getUserBalance);
+      setDataTransaction(result1.userTransactions);
+      setDataUser(result2.userTransactions);
     }
-  }, [events]);
+  }, [userSelected]);
 
-  const setLightOff = useCallback(() => {
-    let LampadinaEvent = events.filter(isLampadinaEvent);
-    let ContatoreEvent = events.filter(isContatoreEvent);
-
-    let actualStatusCont = ContatoreEvent[ContatoreEvent.length - 1];
-    if (LampadinaEvent.length > 0) {
-      let actualStatusLamp = LampadinaEvent[LampadinaEvent.length - 1];
-      if (
-        actualStatusLamp.status === 'LAMPADINA_ACCESA' &&
-        actualStatusCont.status === 'CONTATORE_ACCESO'
-      ) {
-        setEvents([
-          ...events,
-          { status: 'LAMPADINA_SPENTA', timestamp: new Date() },
-        ]);
-      }
-    } else {
-      if (actualStatusCont.status === 'CONTATORE_ACCESO') {
-        setEvents([
-          ...events,
-          { status: 'LAMPADINA_SPENTA', timestamp: new Date() },
-        ]);
-      }
+  let userToPrint: string[] = [];
+  dataUser.items.map((user) => {
+    if (!userToPrint.includes(user.userId)) {
+      userToPrint.push(user.userId);
     }
-  }, [events]);
-
-  const setGenOff = useCallback(() => {
-    let ContatoreEvent = events.filter(isContatoreEvent);
-
-    let actualStatusCont = ContatoreEvent[ContatoreEvent.length - 1];
-
-    if (actualStatusCont.status === 'CONTATORE_ACCESO') {
-      setEvents([
-        ...events,
-        { status: 'CONTATORE_SPENTO', timestamp: new Date() },
-      ]);
-    }
-  }, [events]);
-
-  const setGenOn = useCallback(() => {
-    let ContatoreEvent = events.filter(isContatoreEvent);
-
-    let actualStatusCont = ContatoreEvent[ContatoreEvent.length - 1];
-
-    if (actualStatusCont.status === 'CONTATORE_SPENTO') {
-      setEvents([
-        ...events,
-        { status: 'CONTATORE_ACCESO', timestamp: new Date() },
-      ]);
-    }
-  }, [events]);
-
-  function doCommand(
-    command:
-      | 'SPEGNI_LAMPADINA'
-      | 'ACCENDI_LAMPADINA'
-      | 'SPEGNI_GENERATORE'
-      | 'ACCENDI_GENERATORE',
-  ) {
-    switch (command) {
-      case 'SPEGNI_LAMPADINA':
-        setCommand([
-          ...commands,
-          { command: 'SPEGNI_LAMPADINA', timestamp: new Date() },
-        ]);
-
-        setLightOff();
-        break;
-      case 'ACCENDI_LAMPADINA':
-        setCommand([
-          ...commands,
-          { command: 'ACCENDI_LAMPADINA', timestamp: new Date() },
-        ]);
-
-        setLightOn();
-        break;
-      case 'ACCENDI_GENERATORE':
-        setCommand([
-          ...commands,
-          { command: 'ACCENDI_GENERATORE', timestamp: new Date() },
-        ]);
-
-        setGenOn();
-        break;
-      case 'SPEGNI_GENERATORE':
-        setCommand([
-          ...commands,
-          { command: 'SPEGNI_GENERATORE', timestamp: new Date() },
-        ]);
-
-        setGenOff();
-        break;
-    }
-  }
+  });
 
   return (
     <>
-      <div className="flex-grow justify-evenly flex m-4">
-        <BtnOn onClick={() => doCommand('ACCENDI_LAMPADINA')} />
-        <BtnOff onClick={() => doCommand('SPEGNI_LAMPADINA')} />
-        <button
-          className="border border-black px-3 py-2 mr-2 rounded"
-          onClick={() => {
-            console.log(events, commands);
-          }}
-        >
-          Log
-        </button>
-        <BtnGeneratorOn
-          onClick={() => {
-            doCommand('ACCENDI_GENERATORE');
-          }}
-        />
-        <BtnGeneratorOff
-          onClick={() => {
-            doCommand('SPEGNI_GENERATORE');
-          }}
-        />
-      </div>
-      <Lampada {...events} />
-      {/* <Counter seconds={seconds} /> */}
+      <UsersMonitor
+        userSelected={userSelected}
+        data={userToPrint}
+        onClick={setUserSelected}
+      />
+      <BtnAll onClick={setUserSelected} />
+      <TransactionMonitor data={dataTransaction} />
+      <UsersBalanceMonitor
+        userBalance={userBalance}
+        userSelected={userSelected}
+      />
     </>
   );
 }
 
 export default App;
-/*
-  function isLastMinute(event: Event) {
-    let actualDate = new Date();
-    if (
-      event.timestamp.getTime() > actualDate.getTime() - 60000 &&
-      event.status === 'LAMPADINA_ACCESA'
-    )
-      return event;
-  }
-   
-  useEffect(() => {
-    const counter = setInterval(() => {
-      let lastMinute = events.filter(isLastMinute);
-      if (lastMinute.length > 0) {
-        console.log(lastMinute);
-        let actualDate = new Date();
-        let dif = actualDate.getTime() - lastMinute[0].timestamp.getTime();
-        var differenceBetween = Math.floor(dif / 1000);
-        setSeconds(differenceBetween);
-      } else {
-        setSeconds(0);
-      }
-    }, 1000);
 
-    return () => clearInterval(counter);
-  }, [events]); */
+function UsersBalanceMonitor(props: {
+  userSelected: string;
+  userBalance: number;
+}) {
+  if (props.userSelected !== 'all') {
+    return (
+      <div className="border-cyan-600 border-2 rounded-l-lg m-4 ml-60 mr-0 mt-12 h-20 text-lg flex items-center p-2 shadow-lg">
+        The account balance of {props.userSelected} is {props.userBalance} €
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+}
+
+function UsersMonitor(props: {
+  data: string[];
+  userSelected: string;
+  onClick: (val: string) => void;
+}) {
+  return (
+    <div className="border-cyan-600 border-2 rounded-l-lg shadow-lg m-4 ml-60 mr-0">
+      {props.data.map((user, i) => {
+        return (
+          <UserCard
+            userSelected={props.userSelected}
+            user={user}
+            key={i}
+            onClick={props.onClick}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function UserCard(props: {
+  user: string;
+  userSelected: string;
+  onClick: (val: string) => void;
+}) {
+  const classText = classNames(
+    'flex flex-row-reverse shadow-sm p-6 hover:bg-gray-50 transform transition duration-100 hover:scale-99 cursor-pointer',
+    props.userSelected === props.user ? 'bg-gray-200' : 'bg-transparent',
+  );
+  return (
+    <div className={classText} onClick={() => props.onClick(props.user)}>
+      <div className="text-black text-lg">{props.user}</div>
+    </div>
+  );
+}
+
+function TransactionMonitor(props: { data: UserTransactions }) {
+  return (
+    <div className="border-cyan-600 border-2 rounded-r-lg shadow-lg m-4 mr-60 ml-0">
+      <div className="flex flex-col">
+        {props.data.items.map((transaction, i) => {
+          return <Transaction key={i} transaction={transaction} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+function Transaction(props: { transaction: UserTransaction }) {
+  let dateOperation = DateTime.fromISO(props.transaction.creditDate);
+  const classText = classNames(
+    'my-4 p-2 text-lg',
+    props.transaction.amount > 0 ? 'text-green-500' : 'text-red-500',
+  );
+  return (
+    <div className="flex shadow-sm hover:bg-gray-50 transform transition duration-100 hover:scale-99 cursor-default">
+      <div className="flex flex-col flex-grow justify-around p-1">
+        <div className=" text-gray-200">
+          {dateOperation.toFormat('yyyy LLL dd')}
+        </div>
+        <div className=" text-black">{props.transaction.id}</div>
+      </div>
+
+      <div className={classText}>{props.transaction.amount} €</div>
+    </div>
+  );
+}
+
+function BtnAll(props: { onClick: (val: string) => void }) {
+  return (
+    <div className="flex flex-row-reverse">
+      <button
+        className="border-cyan-600 rounded-md border-2 p-2 text-lg text-red-500 m-4"
+        onClick={() => props.onClick('all')}
+      >
+        Show All
+      </button>
+    </div>
+  );
+}
